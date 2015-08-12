e4a2ba88a9f535e9190e520aa5dd2290fa1ecdb9
koogix <koogix@163.com>
2015-08-13 03:03:40 +0800

    修改CMakeLists.txt，可以使用-D定义相关设置

--- C++/DB/autogen.sh
+++ C++/DB/autogen.sh
@@ -3,4 +3,9 @@
 rm -Rf build
 mkdir build
 
-cd build && cmake -DMYSQL_INC_DIR="/usr/local/include/mysql" .. && make
+cd build && cmake \
+	-DMYSQL_INC_DIR="/usr/local/mysql/include/mysql" \
+	-DMYSQL_LIB_DIR="/usr/local/mysql/lib" \
+	-DREDIS_INC_DIR="/usr/local/redis/inc" \
+	-DREDIS_LIB_DIR="/usr/local/redis/lib" \
+	.. && make
