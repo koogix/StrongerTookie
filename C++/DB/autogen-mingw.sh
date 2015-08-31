cbdb9971a5acb34b87cc2027e4089620c32b7866
koogix <koogix@163.com>
2015-09-01 02:48:38 +0800

    修改cmake文件查找dl库方法，win32系统不使用dl

--- C++/DB/autogen-mingw.sh
+++ C++/DB/autogen-mingw.sh
@@ -5,8 +5,11 @@ mkdir build
 
 cd build && cmake \
 	-G "MSYS Makefiles" \
-	-DUSE_LLVM_CLANG=1 \
-	-DUSE_REDIS=0 \
-	-DMYSQL_INC_DIR="/mingw64/include/mysql" \
-	-DMYSQL_LIB_DIR="/mingw64/lib" \
+	-D USE_LLVM_CLANG=0 \
+	-D USE_REDIS=0 \
+	-D USE_SQLITE3=1 \
+	-D SQLITE3_INC_DIR="/mingw64/include" \
+	-D SQLITE3_LIB_DIR="/mingw64/lib" \
+	-D MYSQL_INC_DIR="/mingw64/include/mysql" \
+	-D MYSQL_LIB_DIR="/mingw64/lib" \
 	.. && make
